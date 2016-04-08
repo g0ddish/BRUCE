e911eed97bb020e205cc955c37c7a98018ff8260
ahughes <ahughes@bimm.com>
2016-04-08 10:48:02 -0400

    Inital bruce

---
+++ app/Http/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/', function () {
+    shell_exec('say -v "Alex" '.escapeshellarg($_POST['say']));
+        echo 'say -v "Alex" '.escapeshellarg($_POST['say']);
+    return view('welcome');
+});
