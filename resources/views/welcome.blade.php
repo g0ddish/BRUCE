0c7b797beeb429e611d6ed1edc37733b501764f6
ahughes <ahughes@bimm.com>
2016-04-08 12:44:58 -0400

    go

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,46 +6,28 @@
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
         <style>
-            html, body {
-                height: 100%;
-            }
-
             body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
                 font-weight: 100;
                 font-family: 'Lato';
             }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
             .title {
+                text-align: center;
                 font-size: 96px;
             }
         </style>
+        <link rel="stylesheet" href="./assets/css/bootstrap.css">
+        <script src="./assets/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <div class="container">
-            <div class="content">
+    <div class="col-md-4 col-md-offset-4">
+        <div class="container-fluid">
                 <div class="title">Bruce</div>
                 <form method="post">
-                    <textarea name="say" cols="30" row="30">
-
-                    </textarea>
-                    <input type="submit"/>
+                    <textarea class="form-control" name="say" cols="30" rows="10"></textarea>
+                    <input class="btn btn-primary" type="submit" style="margin-top:10px;"/>
                 </form>
-            </div>
+        </div>
         </div>
     </body>
 </html>
